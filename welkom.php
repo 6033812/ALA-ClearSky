5fffa25ccb4015115d1eee689ad5a87895984aad
Jay Mancham <114405496+6033812@users.noreply.github.com>
2024-04-04 09:47:41 +0000

    Main  hjvh

---
+++ welkom.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Producten123 Galerij</title>
+    <style>
+        .banner {
+            text-align: center;
+            margin: 20px;
+        }
+        
+        .banner img {
+            width: 100%; /* Pas dit aan naar gewenste breedte */
+            height: auto; /* Behoudt aspectverhouding */
+            max-width: 300px; /* Maximale breedte van de afbeelding */
+        }
+        
+        .banner p {
+            margin-top: 10px;
+            font-size: 16px;
+            color: #333;
+        }
+    </style>
+</head>
+<body>
+<div class="navbar">
+    <a href="welkom.php">Home</a>
+    <a href="product.php">Products</a>
+    <a href="contact.php">Contact</a>
+</div>
+
+<div class="banner">
+    <img src="product1.jpg" alt="Product 1">
+    <p>Product 3</p>
+</div>
+<div class="banner">
+    <img src="product2.jpg" alt="Product 2">
+    <p>Product 2</p>
+</div>
+
+</body>
+</html>
